e4cfc7460496b668a8c23d67a67f4cb6e3925568
Cyrille Meichel <cyrille.meichel@corp.ovh.com>
2018-07-17 14:08:22 +0200

    map as a new component

--- app/src/main/java/noopy/client/testosm/MapFragment.kt
+++ app/src/main/java/noopy/client/testosm/MapFragment.kt
@@ -2,28 +2,22 @@ package noopy.client.testosm
 
 import android.app.Fragment
 import android.content.Context
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import noopy.client.testosm.cache.CacheManager
-import noopy.client.testosm.service.MarkerService
+import noopy.client.testosm.components.MapMonitor
 import org.osmdroid.config.Configuration
-import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
 import org.osmdroid.util.GeoPoint
-import org.osmdroid.util.MapTileIndex
-import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.OverlayItem
 
 class MapFragment : Fragment() {
 
-    private var mapComponent: MapView? = null
+    private var mapComponent: MapMonitor? = null
     private var currentContext: Context? = null
     private var overlayList = ArrayList<OverlayItem>()
 
-    private var boatMarker: Marker? = null
     private var currentPosition: GeoPoint = GeoPoint(0.0,0.0)
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
@@ -36,16 +30,13 @@ class MapFragment : Fragment() {
 
         currentContext = rootView?.context
 
-        mapComponent = rootView?.findViewById(R.id.mapView) as MapView
-
-        configureMap()
-
-
+        mapComponent = rootView?.findViewById(R.id.mapView) as MapMonitor
 
         updateMarkerAngle(12.5)
         currentPosition = GeoPoint(-49.9, 68.5)
         updateMarkerPosition(currentPosition)
-        if (mapComponent!= null) mapComponent!!.getController().setCenter(currentPosition);
+
+        mapComponent?.centerTo(currentPosition)
 
 
         return rootView
@@ -61,59 +52,12 @@ class MapFragment : Fragment() {
         mapComponent?.onResume()
     }
 
-    private fun configureMap() {
-        mapComponent?.setTileSource(object : OnlineTileSourceBase("ArcGis", 0, 18, 256, "",
-                arrayOf("https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/")) {
-            override fun getTileURLString(pMapTileIndex: Long): String {
-
-                return (baseUrl + MapTileIndex.getZoom(pMapTileIndex) + "/" + MapTileIndex.getY(pMapTileIndex) + "/" + MapTileIndex.getX(pMapTileIndex)
-                        + mImageFilenameEnding)
-            }
-        })
-
-        mapComponent?.minZoomLevel = 3.0;
-        mapComponent?.controller?.zoomTo(3.0)
-
-        mapComponent?.setBuiltInZoomControls(false);
-        mapComponent?.setMultiTouchControls(true);
-        addBoatMarker()
-    }
-
-
-    private fun createBoatIcon(angle: Double): BitmapDrawable? {
-        if (currentContext != null) {
-
-            var bmp =  MarkerService(currentContext!!).getBmp(R.drawable.fragments_skipper_boat_marker, 80.0)
-            var bmp2 = MarkerService(currentContext!!).transformBmp(bmp!!, angle, 1.0)
-
-            return BitmapDrawable(resources, bmp2)
-
-        }
-        return null
-    }
-
-    private fun addBoatMarker() {
-        Marker.ENABLE_TEXT_LABELS_WHEN_NO_IMAGE = true
-
-        boatMarker = Marker(mapComponent)
-        boatMarker?.title = "hello world"
-        val markerIcon = createBoatIcon(0.0)
-        boatMarker?.setIcon(markerIcon)
-        boatMarker?.position = GeoPoint(0.0,0.0)
-        mapComponent?.overlays?.add(boatMarker)
-        mapComponent?.invalidate()
-
-    }
-
     private fun updateMarkerAngle(angle: Double) {
-        val markerIcon = createBoatIcon(angle.toDouble())
-        boatMarker?.setIcon(markerIcon)
-        mapComponent?.invalidate()
+        mapComponent?.updateBoatAngle(angle)
     }
 
     private fun updateMarkerPosition(pos: GeoPoint) {
-        boatMarker?.position = pos;
-        mapComponent?.invalidate()
+        mapComponent?.updateBoatPosition(pos)
     }
 
 
---
+++ app/src/main/java/noopy/client/testosm/components/MapMonitor.kt
@@ -0,0 +1,130 @@
+package noopy.client.testosm.components
+
+import android.content.Context
+import android.graphics.drawable.BitmapDrawable
+import android.os.Handler
+import android.util.AttributeSet
+import noopy.client.testosm.R
+import noopy.client.testosm.service.MarkerService
+import org.osmdroid.tileprovider.MapTileProviderBase
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.util.MapTileIndex
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import java.util.regex.Pattern
+
+class MapMonitor : MapView {
+
+    private var boatMarker: Marker = Marker(this)
+
+    constructor(context: Context,
+                tileProvider: MapTileProviderBase,
+                tileRequestCompleteHandler: Handler, attrs: AttributeSet) : super(context, tileProvider, tileRequestCompleteHandler, attrs) {
+        init()
+    }
+
+    constructor(context: Context,
+                tileProvider: MapTileProviderBase,
+                tileRequestCompleteHandler: Handler, attrs: AttributeSet, hardwareAccelerated: Boolean) : super(context, tileProvider, tileRequestCompleteHandler, attrs, hardwareAccelerated) {
+        init()
+    }
+
+    /**
+     * Constructor used by XML layout resource (uses default tile source).
+     */
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context,
+                aTileProvider: MapTileProviderBase) : super(context, aTileProvider) {
+        init()
+    }
+
+    constructor(context: Context,
+                aTileProvider: MapTileProviderBase,
+                tileRequestCompleteHandler: Handler) : super(context, aTileProvider, tileRequestCompleteHandler) {
+        init()
+    }
+
+    fun init() {
+
+        setTileSource(object : OnlineTileSourceBase("ArcGis", 0, 18, 256, "",
+                arrayOf("https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}")) {
+            override fun getTileURLString(pMapTileIndex: Long): String {
+
+                return buildTileUrl(baseUrl, MapTileIndex.getX(pMapTileIndex), MapTileIndex.getY(pMapTileIndex), MapTileIndex.getZoom(pMapTileIndex))
+
+            }
+        })
+
+        minZoomLevel = 3.0
+        controller.zoomTo(3.0)
+
+        setBuiltInZoomControls(false)
+        setMultiTouchControls(true)
+        addBoatMarker()
+
+    }
+
+    private fun addBoatMarker() {
+        Marker.ENABLE_TEXT_LABELS_WHEN_NO_IMAGE = true
+
+        boatMarker.title = "hello world"
+        val markerIcon = createBoatIcon(0.0)
+        boatMarker.setIcon(markerIcon)
+        boatMarker.position = GeoPoint(0.0,0.0)
+        overlays.add(boatMarker)
+        invalidate()
+
+    }
+
+    private fun createBoatIcon(angle: Double): BitmapDrawable {
+        var bmp =  MarkerService(context).getBmp(R.drawable.fragments_skipper_boat_marker, 80.0)
+        var bmp2 = MarkerService(context).transformBmp(bmp!!, angle, 1.0)
+
+        return BitmapDrawable(resources, bmp2)
+    }
+
+    fun updateBoatAngle(angle: Double) {
+        val markerIcon = createBoatIcon(angle)
+        boatMarker.setIcon(markerIcon)
+        invalidate()
+    }
+
+    fun updateBoatPosition(pos: GeoPoint) {
+        boatMarker.position = pos
+        invalidate()
+    }
+
+    fun centerTo(position: GeoPoint) {
+        controller.setCenter(position)
+    }
+
+    companion object {
+        fun buildTileUrl(path: String, x: Int, y: Int, z: Int): String {
+            val pattern = Pattern.compile("\\{([^}]*)\\}")
+            val output = StringBuffer()
+            val matcher = pattern.matcher(path)
+            while (matcher.find()) {
+                val key = matcher.group(1)
+                if (key == "x") {
+                    matcher.appendReplacement(output, "{\$x}")
+                }
+                if (key == "y") {
+                    matcher.appendReplacement(output, "{\$y}")
+                }
+                if (key == "z") {
+                    matcher.appendReplacement(output, "{\$z}")
+                }
+            }
+            matcher.appendTail(output)
+            return output.toString()
+        }
+    }
+}
